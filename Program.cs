94c0d9fea9061f0870e7a4d62cbb13f0c1c97db4
Ville M. Vainio <ville.vainio@basware.com>
2019-03-13 18:42:19 +0200

    parse csv file

--- Program.cs
+++ Program.cs
@@ -3,7 +3,10 @@ using System.IO;
 using Microsoft.SqlServer.TransactSql.ScriptDom;
 using System.Linq;
 using System.Collections.Generic;
-
+using System.Text;
+using System.Text.RegularExpressions;
+using CsvHelper;
+using static System.Console;
 namespace CheckTsql
 {
     struct SqlError
@@ -15,12 +18,30 @@ namespace CheckTsql
     }
     class Program
     {
+        private static TSql130Parser parser = new TSql130Parser(true);
 
+
+
+        static SqlError[] ParseString(string text)
+        {
+
+            var rd = new StringReader(text);
+            parser.Parse(rd, out var rawErrors);
+            return rawErrors.Select(err =>
+                new SqlError
+                {
+                    Line = err.Line,
+                    Message = err.Message
+                }
+            ).ToArray();
+
+        }
         static SqlError[] ParseFile(string fname)
         {
             var textReader = new StreamReader(fname);
-            var parser = new TSql130Parser(true);
+
             var sql = parser.Parse(textReader, out var rawErrors);
+
             var errors = rawErrors.Select(err =>
                 new SqlError
                 {
@@ -30,11 +51,51 @@ namespace CheckTsql
             ).ToArray();
             return errors;
         }
+
+        static void ParseCsvFile(string fname)
+        {
+            var p = new CsvParser(new StreamReader(fname));
+            var good = new List<string>();
+
+            var badFileName = $"{fname}.bad.csv";
+            var writer = new StreamWriter(File.Create(badFileName), Encoding.UTF8);
+
+            int linenum = 0;
+            while (true)
+            {
+                var e = p.Read();
+                linenum++;
+                if (e == null)
+                {
+                    break;
+                }
+                var l = e[0];
+                var errors = ParseString(l);
+                if (errors.Length == 0)
+                {
+                    good.Add(e[0]);
+                }
+                else
+                {
+                    writer.Write(linenum);
+                    writer.Write(": ");
+
+                    writer.WriteLine(l);
+                }
+            }
+            writer.Flush();
+
+        }
         static void Main(string[] args)
         {
             var allFiles = new List<string>();
-
             foreach (var arg in args) {
+                if (arg.EndsWith(".csv"))
+                {
+                    ParseCsvFile(arg);
+                    continue;
+
+                }
                 if (File.Exists(arg)) {
                     allFiles.Add(arg);
                     continue;
